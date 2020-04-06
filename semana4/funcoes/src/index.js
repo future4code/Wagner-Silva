6d2085ea9617802b14981adf931b1b3568aba50a
wcardosos <wagnerdev01@gmail.com>
2020-04-06 18:01:35 -0300

    Exercícios resolvidos.

--- semana4/funcoes/src/index.js
+++ semana4/funcoes/src/index.js
@@ -19,6 +19,7 @@
       retorna o fatorial de array.length.
 */
 
+
 /* RESPOSTAS DAS QUESTÕES DE ESCRITA DE CÓDIGO */
 
 /* EXERCÍCIO 4 */
@@ -38,4 +39,100 @@ function apresentacao(nome, idade, endereco, estudante) {
 
    return "Eu sou " + nome + ", tenho " + idade + " anos, moro em " +
            endereco + " e " + estuda + " estudante.";
+}
+
+
+/* EXERCÍCIO 5 */
+function numeroEmAlgarismoRomano(numero) {
+   console.log(numero)
+   let algarismoRomano = "X";
+   let novoNumero = numero - 10;
+
+   const um = "I";
+   const dois = "II";
+   const tres = "III";
+   const quatro = "IV";
+   const cinco = "V";
+   const seis = cinco + um;
+   const sete = cinco + dois;
+   const oito = cinco + tres;
+   const nove = "IX";
+
+   if(novoNumero >= 10) {
+      algarismoRomano += "X";
+      novoNumero -= 10;
+   }
+   
+   if(novoNumero > 0) {
+      if(novoNumero === 1) {
+         algarismoRomano += um;
+      } else if(novoNumero === 2) {
+         algarismoRomano += dois;
+      } else if(novoNumero === 3) {
+         algarismoRomano += tres;
+      } else if(novoNumero === 4) {
+         algarismoRomano += quatro;
+      } else if(novoNumero === 5) {
+         algarismoRomano += cinco;
+      } else if(novoNumero === 6) {
+         algarismoRomano += seis;
+      } else if(novoNumero === 7) {
+         algarismoRomano += sete;
+      } else if(novoNumero === 8) {
+         algarismoRomano += oito;
+      } else if(novoNumero === 9) {
+         algarismoRomano += nove;
+      }
+   }
+
+   return algarismoRomano;
+}
+
+function seculoQueAnoPertence(ano) {
+   let seculo;
+   if(ano % 100 === 0) {
+      seculo = ano / 100;
+   } else {
+      seculo = (Math.floor(ano / 100) + 1);
+   }
+
+   const algarismoRomano = numeroEmAlgarismoRomano(seculo);
+
+   return "O ano " + ano + " pertence ao século " + algarismoRomano;
+}
+
+
+/* EXERCÍCIO 6 */
+const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22];
+
+/* a. */
+function quantidadeDeElementos(array) {
+   return array.length;
+}
+
+/* b. */
+function par(numero) {
+   return numero % 2 === 0;
+}
+
+/* c. */
+function numerosParesDeUmArray(array) {
+   const arrayDePares = [];
+
+   for(let numero of array) {
+      if(numero % 2 === 0) arrayDePares.push(numero);
+   }
+
+   return arrayDePares;
+}
+
+/* d. */
+function numerosPares(array) {
+   const arrayDePares = [];
+
+   for(let numero of array) {
+      if(par(numero)) arrayDePares.push(numero);
+   }
+
+   return arrayDePares;
 }
