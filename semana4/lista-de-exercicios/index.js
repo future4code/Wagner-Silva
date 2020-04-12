cef9f15ec0c99105056d3c6313bc45db256aa13a
wcardosos <wagnerdev01@gmail.com>
2020-04-12 16:31:35 -0300

    Aula 14 exercicio 2 leitura

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -6,6 +6,10 @@
    1- A função recebe o valor em dólares como argumento, pergunta ao usuário o valor da cotação e
    multiplica o valor em dólares pela cotação, retornando como string, precedido de "R$". Por fim,
    imprime na tela o valor em reais de 100 dólares.
-   
 
+   2- A função recebe um tipo de investimento e o valor a ser investido. Através do switch/case
+   define-se o tipo de investimento, informando erro caso o tipo esteja incorreto. Após a escolha,
+   define-se o valor de investimento de acordo com a opção escolhida e retorna este valor. Por fim,
+   imprime na tela um valor de 150 investido em ações e 200 em tesouro direto.
+   
 */
