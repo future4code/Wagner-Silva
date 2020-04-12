3f50a2cbc2daab2eecbb5b796a81ce12a25f7820
wcardosos <wagnerdev01@gmail.com>
2020-04-12 19:04:18 -0300

    Projeto da semana completo.

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -38,99 +38,99 @@
    undefined no momento da comparação; e segundo que não há nenhuma comparação se o número é par ou não.
    Abaixo está a implementação correta da função.
 */
-// function quantidadeDeNumerosPares(quantidade) {
-//     for(let i = 0; i < quantidade * 2; i += 2) {
-//         console.log(i);
-//     }
-// }
+function quantidadeDeNumerosPares(quantidade) {
+    for(let i = 0; i < quantidade * 2; i += 2) {
+        console.log(i);
+    }
+}
 
 /* EXERCÍCIO 3 */
-// function classificacaoDeTriangulo(a, b, c) {
-//     if(a === b && b === c) {
-//         return "Equilátero";
-//     } else if(a === b || b === c || a === c) {
-//         return "Isósceles";
-//     } else {
-//         return "Escaleno";
-//     }
-// }
+function classificacaoDeTriangulo(a, b, c) {
+    if(a === b && b === c) {
+        return "Equilátero";
+    } else if(a === b || b === c || a === c) {
+        return "Isósceles";
+    } else {
+        return "Escaleno";
+    }
+}
 
 /* EXERCÍCIO 4 */
-// function maior(a, b) {
-//     if(a > b) return a;
-//     else return b;
-// }
+function maior(a, b) {
+    if(a > b) return a;
+    else return b;
+}
 
-// function diviseis(a, b) {
-//     return a % b === 0;
-// }
+function diviseis(a, b) {
+    return a % b === 0;
+}
 
-// function diferenca(a, b) {
-//     return Math.abs(a -b);
-// }
+function diferenca(a, b) {
+    return Math.abs(a -b);
+}
 
-// function exercicio4(a, b) {
-//     console.log(`ENTRADA:\n${a} e ${b}\n`);
-//     console.log(`SAÍDA:\nO maior é ${maior(a, b)}`);
+function exercicio4(a, b) {
+    console.log(`ENTRADA:\n${a} e ${b}\n`);
+    console.log(`SAÍDA:\nO maior é ${maior(a, b)}`);
 
-//     if(diviseis(a, b)) {
-//         console.log(`${a} é divisível por ${b}`);
-//     } else {
-//         console.log(`${a} não é divisível por ${b}`);
-//     }
+    if(diviseis(a, b)) {
+        console.log(`${a} é divisível por ${b}`);
+    } else {
+        console.log(`${a} não é divisível por ${b}`);
+    }
 
-//     if(diviseis(b, a)) {
-//         console.log(`${b} é divisível por ${a}`);
-//     } else {
-//         console.log(`${b} não é divisível por ${a}`);
-//     }
+    if(diviseis(b, a)) {
+        console.log(`${b} é divisível por ${a}`);
+    } else {
+        console.log(`${b} não é divisível por ${a}`);
+    }
 
-//     console.log(`A diferença entre eles é ${diferenca(a, b)}`);
-// }
+    console.log(`A diferença entre eles é ${diferenca(a, b)}`);
+}
 
 
 /* AULA 15 - Objetos, Strings e Arrays */
 /* EXERCÍCIOS DE FUNÇÕES */
 /* EXERCÍCIO 1 */
-// function segundoMaior(numeros) {
-//     let maior = Math.max(...numeros);
-//     let segundoMaior = Math.min(...numeros);
+function segundoMaior(numeros) {
+    let maior = Math.max(...numeros);
+    let segundoMaior = Math.min(...numeros);
 
-//     for(let numero of numeros) {
-//         if(numero > segundoMaior && numero < maior) {
-//             segundoMaior = numero;
-//         }
-//     }
+    for(let numero of numeros) {
+        if(numero > segundoMaior && numero < maior) {
+            segundoMaior = numero;
+        }
+    }
 
-//     return segundoMaior;
-// }
+    return segundoMaior;
+}
 
-// function segundoMenor(numeros) {
-//     let menor = Math.min(...numeros);
-//     let segundoMenor = Math.max(...numeros);
+function segundoMenor(numeros) {
+    let menor = Math.min(...numeros);
+    let segundoMenor = Math.max(...numeros);
 
-//     for(let numero of numeros) {
-//         if(numero < segundoMenor && numero > menor) {
-//             segundoMenor = numero;
-//         }
-//     }
+    for(let numero of numeros) {
+        if(numero < segundoMenor && numero > menor) {
+            segundoMenor = numero;
+        }
+    }
 
-//     return segundoMenor;
-// }
+    return segundoMenor;
+}
 
-// function exercicio1(numeros) {
-//     console.log(segundoMaior(numeros));
-//     console.log(segundoMenor(numeros));
-// }
+function exercicio1(numeros) {
+    console.log(segundoMaior(numeros));
+    console.log(segundoMenor(numeros));
+}
 
-// exercicio1([4,1,8,9,3,2,10]);
+exercicio1([4,1,8,9,3,2,10]);
 
 /* EXERCÍCIO 2 */
-// const exercicio2 = () => {
-//     alert("Hello Labenu");
-// };
+const exercicio2 = () => {
+    alert("Hello Labenu");
+};
 
-// exercicio2();
+exercicio2();
 
 /* EXERCÍCIOS DE OBJETOS */
 /* EXERCÍCIO 1
@@ -140,47 +140,47 @@
 */
 
 /* EXERCÍCIO 2 */
-// function criaRetangulo(lado1, lado2) {
-//     return {
-//         largura: lado1,
-//         altura: lado2,
-//         perimetro: 2 * (lado1 + lado2),
-//         area: lado1 * lado2
-//     }
-// }
+function criaRetangulo(lado1, lado2) {
+    return {
+        largura: lado1,
+        altura: lado2,
+        perimetro: 2 * (lado1 + lado2),
+        area: lado1 * lado2
+    }
+}
 
 /* EXERCICIO 3 */
-// const filme = {
-//     titulo: "O Irlandês",
-//     ano: 2019,
-//     diretor: "Martin Scorsese",
-//     atores: ["Robert De Niro", "Al Pacino", "Joe Pesci"]
-// }
+const filme = {
+    titulo: "O Irlandês",
+    ano: 2019,
+    diretor: "Martin Scorsese",
+    atores: ["Robert De Niro", "Al Pacino", "Joe Pesci"]
+}
 
-// function filmeFavorito(filme) {
-//     console.log(`Venha assistir ao filme ${filme.titulo}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores.toString()}.`);
-// }
+function filmeFavorito(filme) {
+    console.log(`Venha assistir ao filme ${filme.titulo}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores.toString()}.`);
+}
 
-// filmeFavorito(filme);
+filmeFavorito(filme);
 
 /* EXERCÍCIO 4 */
-// const pessoa = {
-//     nome: "Wagner",
-//     idade: 21,
-//     email: "wagner@email.com",
-//     endereco: "Cuitegi-PB",
-// }
+const pessoa = {
+    nome: "Wagner",
+    idade: 21,
+    email: "wagner@email.com",
+    endereco: "Cuitegi-PB",
+}
 
-// function anonimizarPessoa(pessoa) {
-//     return {
-//         ...pessoa,
-//         nome: "ANÔNIMO"
-//     }
-// }
+function anonimizarPessoa(pessoa) {
+    return {
+        ...pessoa,
+        nome: "ANÔNIMO"
+    }
+}
 
-// novoObj = anonimizarPessoa(pessoa);
-// console.log(pessoa);
-// console.log(novoObj);
+novoObj = anonimizarPessoa(pessoa);
+console.log(pessoa);
+console.log(novoObj);
 
 
 /* AULA 16 - CALLBACK E FUNÇÕES DE ARRAY */
@@ -188,114 +188,114 @@
    Antes das funções de array podíamos iterar através de while, for ou for...of.
    Abaixo há um exemplo.
 */
-// function iteracaoEmArrays() {
-//     const array = [1, 2, 3];
+function iteracaoEmArrays() {
+    const array = [1, 2, 3];
 
-//     /* Iteração com while */
-//     let n = 0;
-//     while(n < array.length) {
-//         console.log(array[n]);
-//         n++;
-//     }
+    /* Iteração com while */
+    let n = 0;
+    while(n < array.length) {
+        console.log(array[n]);
+        n++;
+    }
 
-//     /* Iteração com for */
-//     for(let i = 0; i < array.length; i++) {
-//         console.log(array[i]);
-//     }
+    /* Iteração com for */
+    for(let i = 0; i < array.length; i++) {
+        console.log(array[i]);
+    }
 
-//     /* Iteração com for..of */
-//     for(let elemento of array) {
-//         console.log(elemento);
-//     }
-// }
+    /* Iteração com for..of */
+    for(let elemento of array) {
+        console.log(elemento);
+    }
+}
 
 /* EXERCÍCIO 2 */
-// const pessoas = [
-// 	{ nome: "Pedro", idade: 20 },
-// 	{ nome: "João", idade: 10 },
-// 	{ nome: "Paula", idade: 12 },
-// 	{ nome: "Artur", idade: 89 } 
-// ];
+const pessoas = [
+	{ nome: "Pedro", idade: 20 },
+	{ nome: "João", idade: 10 },
+	{ nome: "Paula", idade: 12 },
+	{ nome: "Artur", idade: 89 } 
+];
 
-// /* a. */
-// const adultos = pessoas.filter(pessoa => {
-//     return pessoa.idade >= 20;
-// });
+/* a. */
+const adultos = pessoas.filter(pessoa => {
+    return pessoa.idade >= 20;
+});
 
-// /* b. */
-// const criancasAdolescentes = pessoas.filter(pessoa => {
-//     return pessoa.idade < 20;
-// });
+/* b. */
+const criancasAdolescentes = pessoas.filter(pessoa => {
+    return pessoa.idade < 20;
+});
 
 /* EXERCÍCIO 3 */
-// const array = [1, 2, 3, 4, 5, 6];
+const array = [1, 2, 3, 4, 5, 6];
 
-// /* a. */
-// const arrayMultiplicadoPor2 = array.map(elemento => {
-//     return elemento * 2;
-// });
+/* a. */
+const arrayMultiplicadoPor2 = array.map(elemento => {
+    return elemento * 2;
+});
 
-// /* b. */
-// const arrayMultiplicadoPor3EmString = array.map(elemento => {
-//     return `${elemento * 3}`;
-// });
+/* b. */
+const arrayMultiplicadoPor3EmString = array.map(elemento => {
+    return `${elemento * 3}`;
+});
 
-// /* c. */
-// const arrayParesImpares = array.map(elemento => {
-//     if(elemento % 2 === 0) {
-//         return `${elemento} é par`;
-//     } else {
-//         return `${elemento} é ímpar`;
-//     }
-// });
+/* c. */
+const arrayParesImpares = array.map(elemento => {
+    if(elemento % 2 === 0) {
+        return `${elemento} é par`;
+    } else {
+        return `${elemento} é ímpar`;
+    }
+});
 
 /* EXERCÍCIO 4 */
-// const pessoas = [
-// 	{ nome: "Paula", idade: 12, altura: 1.8},
-// 	{ nome: "João", idade: 20, altura: 1.3},
-// 	{ nome: "Pedro", idade: 15, altura: 1.9},
-// 	{ nome: "Luciano", idade: 22, altura: 1.8},
-// 	{ nome: "Artur", idade: 10, altura: 1.2},
-// 	{ nome: "Soter", idade: 70, altura: 1.9}
-// ];
+const pessoas = [
+	{ nome: "Paula", idade: 12, altura: 1.8},
+	{ nome: "João", idade: 20, altura: 1.3},
+	{ nome: "Pedro", idade: 15, altura: 1.9},
+	{ nome: "Luciano", idade: 22, altura: 1.8},
+	{ nome: "Artur", idade: 10, altura: 1.2},
+	{ nome: "Soter", idade: 70, altura: 1.9}
+];
 
-// /* a. */
-// const pessoasPermitidas = pessoas.filter(pessoa => {
-//     if(pessoa.altura >= 1.5 && pessoa.idade > 14 && pessoa.idade < 60) {
-//         return pessoa;
-//     }
-// });
+/* a. */
+const pessoasPermitidas = pessoas.filter(pessoa => {
+    if(pessoa.altura >= 1.5 && pessoa.idade > 14 && pessoa.idade < 60) {
+        return pessoa;
+    }
+});
 
-// /* b. */
-// const pessoasNaoPermitidas = pessoas.filter(pessoa => {
-//     if(!(pessoa.altura >= 1.5 && pessoa.idade > 14 && pessoa.idade < 60)) {
-//         return pessoa;
-//     }
-// });
+/* b. */
+const pessoasNaoPermitidas = pessoas.filter(pessoa => {
+    if(!(pessoa.altura >= 1.5 && pessoa.idade > 14 && pessoa.idade < 60)) {
+        return pessoa;
+    }
+});
 
 /* EXERCÍCIO 5 */
-// const consultas = [
-// 	{ nome: "João", genero: "masculino", cancelada: true, dataDaConsulta: "01/10/2019" },
-// 	{ nome: "Pedro", genero: "masculino", cancelada: false, dataDaConsulta: "02/10/2019" },
-// 	{ nome: "Paula", genero: "feminino", cancelada: true, dataDaConsulta: "03/11/2019" },
-// 	{ nome: "Márcia", genero: "feminino", cancelada: false, dataDaConsulta: "04/11/2019" }
-// ];
+const consultas = [
+	{ nome: "João", genero: "masculino", cancelada: true, dataDaConsulta: "01/10/2019" },
+	{ nome: "Pedro", genero: "masculino", cancelada: false, dataDaConsulta: "02/10/2019" },
+	{ nome: "Paula", genero: "feminino", cancelada: true, dataDaConsulta: "03/11/2019" },
+	{ nome: "Márcia", genero: "feminino", cancelada: false, dataDaConsulta: "04/11/2019" }
+];
 
-// const emails = consultas.map(consulta => {
-//     if(consulta.genero === "feminino") {
-//         if(consulta.cancelada) {
-//             return `Olá, Sra. ${consulta.nome}. Infelizmente, sua consulta marcada para o dia ${consulta.dataDaConsulta} foi cancelada. Se quiser, pode entrar em contato conosco para remarcá-la`;
-//         } else {
-//             return `Olá, Sra.  ${consulta.nome}. Estamos enviando esta mensagem para lembrá-la da sua consulta no dia ${consulta.dataDaConsulta}. Por favor, acuse o recebimento deste e-mail.`;
-//         }
-//     } else {
-//         if(consulta.cancelada) {
-//             return `Olá, Sr. ${consulta.nome}. Infelizmente, sua consulta marcada para o dia ${consulta.dataDaConsulta} foi cancelada. Se quiser, pode entrar em contato conosco para remarcá-la`;
-//         } else {
-//             return `Olá, Sr. ${consulta.nome}. Estamos enviando esta mensagem para lembrá-lo da sua consulta no dia ${consulta.dataDaConsulta}. Por favor, acuse o recebimento deste e-mail.`;
-//         }
-//     }
-// });
+const emails = consultas.map(consulta => {
+    if(consulta.genero === "feminino") {
+        if(consulta.cancelada) {
+            return `Olá, Sra. ${consulta.nome}. Infelizmente, sua consulta marcada para o dia ${consulta.dataDaConsulta} foi cancelada. Se quiser, pode entrar em contato conosco para remarcá-la`;
+        } else {
+            return `Olá, Sra.  ${consulta.nome}. Estamos enviando esta mensagem para lembrá-la da sua consulta no dia ${consulta.dataDaConsulta}. Por favor, acuse o recebimento deste e-mail.`;
+        }
+    } else {
+        if(consulta.cancelada) {
+            return `Olá, Sr. ${consulta.nome}. Infelizmente, sua consulta marcada para o dia ${consulta.dataDaConsulta} foi cancelada. Se quiser, pode entrar em contato conosco para remarcá-la`;
+        } else {
+            return `Olá, Sr. ${consulta.nome}. Estamos enviando esta mensagem para lembrá-lo da sua consulta no dia ${consulta.dataDaConsulta}. Por favor, acuse o recebimento deste e-mail.`;
+        }
+    }
+});
 
 /* EXERCÍCIO 6 */
 const contas = [
