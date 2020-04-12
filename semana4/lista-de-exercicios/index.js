c5bc8e6a084db4d78940d56a0e342c1afbc33d16
wcardosos <wagnerdev01@gmail.com>
2020-04-12 16:30:10 -0300

    Aula 14 exercicio 1 leitura

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,11 @@
+/* AULA 14 - DOM 
+
+   
+   EXERCÍCIOS DE LEITURA DE CÓDIGO
+
+   1- A função recebe o valor em dólares como argumento, pergunta ao usuário o valor da cotação e
+   multiplica o valor em dólares pela cotação, retornando como string, precedido de "R$". Por fim,
+   imprime na tela o valor em reais de 100 dólares.
+   
+
+*/
