c7cefc8828b45dec0670e5e773d54e73cffd3eba
wcardosos <wagnerdev01@gmail.com>
2020-04-16 16:49:19 -0300

    Exercício incompleto.

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import './App.css';
 import Post from './components/Post/Post';
+import styled from 'styled-components';
+
+const NovoPost = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 60px;
+`
 
 class App extends React.Component {
+  state = {
+    posts: [{
+      nomeUsuario: 'paulinha',
+      fotoUsuario: 'https://picsum.photos/50/50?random=1',
+      fotoPost: 'https://picsum.photos/50/50?random=2'
+    },
+    {
+      nomeUsuario: '_labenu',
+      fotoUsuario: 'https://picsum.photos/50/50?random=3',
+      fotoPost: 'https://picsum.photos/50/50?random=4'
+    },
+    {
+      nomeUsuario: 'wcardosos_',
+      fotoUsuario: 'https://picsum.photos/50/50?random=5',
+      fotoPost: 'https://picsum.photos/50/50?random=6'
+    }
+  ]
+  }
+
   render() {
+    const listaDePosts = this.state.posts.map( post => {
+      return <Post
+               nomeUsuario={post.nomeUsuario}
+               fotoUsuario={post.fotoUsuario}
+               fotoPost={post.fotoPost}
+             />
+    });
+
     return (
       <div className={'app-container'}>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50?random=1'}
-          fotoPost={'https://picsum.photos/200/150?random=2'}
-        />
-        <Post
-          nomeUsuario={'_labenu'}
-          fotoUsuario={'https://picsum.photos/50/50?random=3'}
-          fotoPost={'https://picsum.photos/200/150?random=4'}
-        />
-        <Post
-          nomeUsuario={'wcardosos_'}
-          fotoUsuario={'https://picsum.photos/50/50?random=5'}
-          fotoPost={'https://picsum.photos/200/150?random=6'}
-        />
+        {listaDePosts}
       </div>
     );
   }
