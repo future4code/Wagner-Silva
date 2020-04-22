f58b676f20b169d7c055c5dfe8058993576a624f
Wagner Cardoso <wcardosos3421@gmail.com>
2020-04-22 19:56:32 -0300

    Exercício quase completo.

---
+++ semana6/labenu-forms/build/precache-manifest.50a7639512232dd164eeeda6c536d32c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27a61b042a2aec096f4f75559ede318b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c4955c2e6ff5c22e250",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4691f90f5b7273a51b95",
+    "url": "/static/js/2.e897a0e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e897a0e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c4955c2e6ff5c22e250",
+    "url": "/static/js/main.c6eb5bb3.chunk.js"
+  },
+  {
+    "revision": "3a806352cef03184e653",
+    "url": "/static/js/runtime-main.841c3de0.js"
+  }
+]);
