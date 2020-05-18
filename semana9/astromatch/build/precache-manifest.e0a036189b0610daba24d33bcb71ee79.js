98b0eb33f555145b29a0ef2e72eba67ad6f9b6fb
Wagner Cardoso <wcardosos3421@gmail.com>
2020-05-18 14:03:49 -0300

    Configurações iniciais do projeto

---
+++ semana9/astromatch/build/precache-manifest.e0a036189b0610daba24d33bcb71ee79.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "047363b35ec8daeb0329b341221d08c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fb6dccc09bdf64b77e6",
+    "url": "/static/css/main.33b020a5.chunk.css"
+  },
+  {
+    "revision": "a481dd5c4c9799b61fcd",
+    "url": "/static/js/2.3380f27f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.3380f27f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fb6dccc09bdf64b77e6",
+    "url": "/static/js/main.01e0c51d.chunk.js"
+  },
+  {
+    "revision": "277db97ec78b11b5b3f2",
+    "url": "/static/js/runtime-main.c7528d17.js"
+  },
+  {
+    "revision": "7205907268c0dc4e6519fd8fceee54c1",
+    "url": "/static/media/astromatch.72059072.svg"
+  },
+  {
+    "revision": "5a67bd327075d0c63100f2fde86bf885",
+    "url": "/static/media/couple_one.5a67bd32.svg"
+  },
+  {
+    "revision": "04d8375a757cd686d661aaf4b2c54282",
+    "url": "/static/media/couple_three.04d8375a.svg"
+  },
+  {
+    "revision": "6cfea2fda24b5b2c3bf3d240a7e5cef7",
+    "url": "/static/media/couple_two.6cfea2fd.svg"
+  }
+]);
