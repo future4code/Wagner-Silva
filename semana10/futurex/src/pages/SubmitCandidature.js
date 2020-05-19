f2998c2d795edb82b656b49455821ab42f10bb52
Wagner Cardoso <wcardosos3421@gmail.com>
2020-05-19 14:44:50 -0300

    Página de resposta às candidaturas adicionada

---
+++ semana10/futurex/src/pages/SubmitCandidature.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Header from '../components/Header';
+import colors from '../utils/colors';
+import styled from 'styled-components';
+
+const SubmitCandidature = () => {}
+
+export default SubmitCandidature;
