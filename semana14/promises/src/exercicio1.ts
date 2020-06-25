14ec4631c357189b6d8e6e8cf22532c6229c0e83
Wagner Cardoso <wcardosos3421@gmail.com>
2020-06-25 14:09:27 -0300

    Exercício 2 resolvido

--- semana14/promises/src/exercicio1.ts
+++ semana14/promises/src/exercicio1.ts
@@ -2,11 +2,11 @@ import axios from 'axios';
 import { utils } from './utils';
 
 /* 
- *  a. o endpoint que deve ser utilizado é:
+ *  a. O endpoint que deve ser utilizado é:
  *      https://us-central1-labenu-apis.cloudfunctions.net/labenews/subscribers/all
- *  b. da seguinte maneira:
+ *  b. Da seguinte maneira:
  *      async function nome_funcao(): any[] {}
- *  c. implementação abaixo.
+ *  c. Implementação abaixo.
  */
 
 async function getAllSubscribers(): Promise<any[]> {
---
+++ semana14/promises/src/exercicio2.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { utils } from './utils';
+
+/* 
+ *  a. A diferença, na implementação, é sutil: o tipo de retorno é uma Promise de algum
+ *       tipo estabelecido.
+ *  b. Implementação abaixo.
+ */
+
+const getAllSubscribers = async (): Promise<any[]> => {
+    const subscribersRequest = await axios.get(`${utils.BASE_URL}/subscribers/all`);
+
+    return subscribersRequest.data;
+}
+
+const main = async (): Promise<void> => {
+    console.log(await getAllSubscribers());
+}
+
+main();
