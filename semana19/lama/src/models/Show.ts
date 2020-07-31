44742fc316cce192f25eece5683ebe0de89efd25
Wagner Cardoso <wcardosos3421@gmail.com>
2020-07-31 16:37:23 -0300

    Model de show adicionado

--- semana19/lama/src/models/Show.ts
+++ semana19/lama/src/models/Show.ts
@@ -1,1 +1,32 @@
 import { Moment } from 'moment';
+import { Band } from './Band';
+
+export class Show {
+    constructor(
+        private id: string,
+        private weekDay: string,
+        private startTime: Moment,
+        private endTime: Moment,
+        private band: Band
+    ) {}
+
+    public getId(): string {
+        return this.id;
+    }
+
+    public getWeekDay(): string {
+        return this.weekDay;
+    }
+
+    public getStartTime(): Moment {
+        return this.startTime;
+    }
+
+    public getEndTime(): Moment {
+        return this.endTime;
+    }
+
+    public getBand(): Band {
+        return this.band;
+    }
+}
